54c6742dbfa348f11488e555dc8272b031a753a5
jean bauch <jean.bauch06@gmail.com>
2022-05-29 00:05:12 -0300

    feat[mobile]: create product card mobile

---
+++ components/atoms/ButtonShoppingCartProduct.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex justify-center items-center absolute bottom-10 -right-6 w-12 h-12 bg-primary-green-dark rounded-full text-[#FFF]">
+    <ShoppingCartIcon class="h-6 w-6 text-current relative z-10" />
+  </div>
+</template>
+
+<script setup>
+import { ShoppingCartIcon } from "@heroicons/vue/solid";
+
+</script>
---
+++ components/organisms/CardProduct.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="w-full h-80 flex items-end">
+    <div class="w-[90%] h-[80%] pl-4 pr-6 bg-[#FFF] relative z-20 rounded-xl shadow-md">
+      <div class="flex justify-center relative -top-1/4">
+        <img src="/bg-complement/item-example.svg" class="drop-shadow-xl" alt="">
+      </div>
+      <div class="absolute top-6 left-3">
+        <img src="/icons/icon-imunity.svg" class="-rotate-12 opacity-80" width="28" height="28" alt="Icone para representar que o produto selecionado se trate da categoria Imunidade">
+      </div>
+      <article class="relative z-30 -top-1/4">
+        <h4 class="text-base text-dark-normal font-semibold mb-4">
+          Green Tea Long
+        </h4>
+
+        <div class="flex flex-col gap-2 w-[85%]">
+          <div class="flex justify-between text-sm">
+            <span class="text-secondary-green-gray-dark font-normal">Caterogia:</span>
+            <p class="text-dark-normal font-light">
+              Imunity
+            </p>
+          </div>
+          <div class="flex justify-between text-sm">
+            <span class="text-secondary-green-gray-dark font-normal">Efeito:</span>
+            <p class="text-dark-normal font-light">
+              Imunity
+            </p>
+          </div>
+        </div>
+
+        <div class="flex justify-center items-center mt-4">
+          <h4 class="text-lg text-dark-normal font-semibold mb-4">
+            R$12,90
+          </h4>
+        </div>
+      </article>
+
+      <div class="absolute z-20 bottom-2 left-0 w-full h-8 bg-[#FFF] rounded-b-xl" />
+      <div class="absolute z-10 bottom-0 left-0 w-full h-8 bg-primary-green-dark rounded-b-xl" />
+
+      <atoms-button-shopping-cart-product />
+      <atoms-button-quick-view-product />
+    </div>
+  </div>
+</template>
