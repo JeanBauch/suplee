147eb7642f4eba88dbd186569eac4d51301d02bf
jean bauch <jean.bauch06@gmail.com>
2022-08-20 19:13:11 -0300

    feat: create new page-static-edit-user

---
+++ components/atoms/InputBottomLine.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="w-full pb-1 relative flex items-center after:content-[''] after:w-full after:h-[1px] after:left-0 after:bottom-0 after:absolute after:bg-primary-olivia-dark after:rounded-full">
+    <label
+      :for="propsInputButtomLine.inputKey"
+      :class="small ? 'text-base' : 'text-lg lg:text-xl'"
+      class="font-semibold text-primary-olivia-dark"
+    >
+      {{ propsInputButtomLine.label }}:
+    </label>
+    <input
+      :id="propsInputButtomLine.inputKey"
+      type="text"
+      :name="propsInputButtomLine.inputKey"
+      :class="[
+        propsInputButtomLine.isModified ? 'text-dark-normal' : 'text-secondary-green-gray-dark',
+        propsInputButtomLine.small ? 'text-base' : 'text-base lg:text-lg',
+        propsInputButtomLine.small ? 'text-right' : 'text-left',
+        propsInputButtomLine.small ? 'mr-0' : 'mr-8',
+      ]"
+      class="bg-transparent outline-none ml-4 w-full"
+      :value="userComputed.value"
+      :disabled="!propsInputButtomLine.isModified"
+    >
+    <PencilIcon v-if="(propsInputButtomLine.isModified && !small)" class="absolute right-0 h-6 w-6 text-primary-olivia-dark z-10" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { PencilIcon } from "@heroicons/vue/outline";
+
+const propsInputButtomLine = defineProps<{
+  isModified: boolean;
+  label: string;
+  inputKey: string;
+  small?: boolean;
+}>();
+
+const userComputed = computed(() => ({
+  value: propsInputButtomLine.label === "Nome"
+    ? "Jean Augusto Bauch"
+    : propsInputButtomLine.label === "CPF"
+      ? "468.675.618-96"
+      : propsInputButtomLine.label === "Email"
+        ? "jean.bauch06@gmail.com"
+        : propsInputButtomLine.label === "Celular"
+          ? "11 99999-9999"
+          : ""
+}));
+
+</script>
---
+++ pages/usuario/editar-perfil/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="w-screen min-h-screen bg-complement-background-soft relative z-0 overflow-hidden flex justify-center items-center">
+    <atoms-bg-hero />
+    <Html lang="pt-br">
+      <Head>
+        <Title>Suplee - editar usuário</Title>
+        <Meta name="description" content="Página dedicada ao usuário cadastrar ou logar com uma conta existente na Suplee!" />
+      </Head>
+    </Html>
+    <organisms-edit-user />
+  </div>
+</template>
